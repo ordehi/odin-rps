5874c8b0c9cf08b424d420c9e9e964aa88151108
Joshua Ordehi <mev.dev.kar@gmail.com>
2022-10-02 13:13:07 -0400

    add computer choice function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert('working');
+console.log('Welcome to Rock Paper Scissors');
+
+const choices = ['Rock', 'Paper', 'Scissors'];
+
+function getRandomIntRange(upperLimit) {
+  return Math.floor(Math.random() * upperLimit);
+}
+
+function getComputerChoice() {
+  return choices[getRandomIntRange(choices.length)];
+}
